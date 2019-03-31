c3633f1119a9d63fffd61e3a0f2397e95d208376
cychust <u201614719@hust.edu.cn>
2019-03-31 23:48:04 +0800

    与后台进行登录测试

--- app.js
+++ app.js
@@ -6,7 +6,36 @@ App({
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
 
-    // 登录
+    console.log("aaa")
+    let loginFlag=wx.getStorageSync('jwt');
+    if (loginFlag){
+      wx.checkSession({
+        success:function(){
+
+        },
+        fail:function(){
+          // this.doLogin()
+        }
+      });
+    }else{
+      // 登录
+      // this.doLogin()
+
+    }
+    // wx.getUserInfo({
+    //   success: res => {
+    //     // 可以将 res 发送给后台解码出 unionId
+    //     this.globalData.userInfo = res.userInfo
+    //     console.log(res)
+    //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+    //     // 所以此处加入 callback 以防止这种情况
+    //     if (this.userInfoReadyCallback) {
+    //       this.userInfoReadyCallback(res)
+    //     }
+    //   }
+    // })
+  },
+  doLogin:function(){
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
@@ -25,32 +54,13 @@ App({
           header: {
             'content-type': 'application/json'
           },
-          success: function(res) {
+          success: function (res) {
             //从数据库获取用户信息
-            console.log(res)
+            console.log(res.data.data["jwt_token"])
+            wx.setStorageSync("jwt", res.data.data["jwt_token"])
             console.log("插入小程序登录用户信息成功！");
           }
         })
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-              console.log(res)
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
-            }
-          })
-        }
       }
     })
   },
