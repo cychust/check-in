b743822b97468dd743a20ec21fa76ac97a57f93b
cychust <u201614719@hust.edu.cn>
2019-04-01 00:27:22 +0800

    测试登录端口

--- app.js
+++ app.js
@@ -5,35 +5,6 @@ App({
     var logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
-
-    console.log("aaa")
-    let loginFlag=wx.getStorageSync('jwt');
-    if (loginFlag){
-      wx.checkSession({
-        success:function(){
-
-        },
-        fail:function(){
-          // this.doLogin()
-        }
-      });
-    }else{
-      // 登录
-      // this.doLogin()
-
-    }
-    // wx.getUserInfo({
-    //   success: res => {
-    //     // 可以将 res 发送给后台解码出 unionId
-    //     this.globalData.userInfo = res.userInfo
-    //     console.log(res)
-    //     // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-    //     // 所以此处加入 callback 以防止这种情况
-    //     if (this.userInfoReadyCallback) {
-    //       this.userInfoReadyCallback(res)
-    //     }
-    //   }
-    // })
   },
   doLogin:function(){
     wx.login({
