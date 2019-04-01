8708f4e3710965c1d4e2bd00e3831abcc80506fc
cychust <u201614719@hust.edu.cn>
2019-04-01 19:07:25 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,212 +3,217 @@
 const app = getApp()
 
 Page({
-  data: {
-    show: false,
-    motto: 'Hello World',
-    userInfo: {},
-    groupsInfo: [],
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    joined: [{
-      name: "嗯",
-      image: "/images/userinfo_bg.png",
-      number: 0,
-      title: "每日英语"
-    }],
-    //判断小程序的API，回调，参数，组件等是否在当前版本可用。
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    isHide: false
-  },
-  //事件处理函数
-  onChange(event) {
-    wx.showToast({
-      title: 'cccc',
-      icon: 'none'
-    });
-  },
-  moreBtnClicked: function(e) {
-    console.log(e)
-    var that=this
-    // wx.showToast({
-    //   title: e.currentTarget.dataset.item.title
-    // })
-    var jwt = wx.getStorageSync('jwt_token')
-    wx.request({
-      url: 'http://127.0.0.1:3000/api/v1/group/delete',
-      data: {
-        "group_id":e.currentTarget.dataset.item.id,
-        "owner_id": e.currentTarget.dataset.item.owner_id
-      },
-      header: {
-        'content-type': 'application/json',
-        'authorization': jwt,
-      },
-      method: 'POST',
-      dataType: 'json',
-      responseType: 'text',
-      success: function(res) {
-        console.log(res)
-        that.getGroupsInfo()
-      },
-      fail: function(res) {},
-      complete: function(res) {},
-    })
-
-    this.popupOpen();
-  },
-  popupOpen() {
-    this.setData({
-      show: true
-    })
-  },
-  popupClose() {
-    this.setData({
-      show: false
-    })
-  },
-  getGroupsInfo: function() {
-    var that = this;
-    var jwt = wx.getStorageSync('jwt_token')
-    console.log("11111")
-    wx.request({
-      url: 'http://127.0.0.1:3000/api/v1/group/list',
-      data: '',
-      header: {
-        'content-type': 'application/json',
-        'authorization': jwt,
-      },
-      method: 'GET',
-      dataType: 'json',
-      responseType: 'text',
-      success: function(res) {
-        console.log(res)
-        that.setData({
-          groupsInfo: res.data.data.own_groups
-        })
-        console.log(res.data.data.own_groups)
-      },
-      fail: function(res) {},
-      complete: function(res) {
-        wx.hideLoading()
-      },
-    })
-  },
-  doLogin: function(data) {
-    var that = this
-    console.log(data)
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        // console.log(res.code)
-        wx.request({
-          url: 'http://127.0.0.1:3000/api/v1/login',
-          method: "POST",
-          data: {
-            "code": res.code,
-            "userInfo": data.userInfo,
-            "rawData": data.rawData,
-            "signature": data.signature,
-            "encryptedData": data.encryptedData,
-            "iv": data.iv
-          },
-          header: {
-            'content-type': 'application/json'
-          },
-          success: function(res) {
-            //从数据库获取用户信息
-            console.log(res)
-            console.log(res.data.data["jwt_token"])
-            wx.setStorageSync("jwt_token", "Bearer " + res.data.data["jwt_token"])
-            console.log("插入小程序登录用户信息成功！");
-            that.getGroupsInfo()
-          },
-		  fail: function(res){
-			  wx.hideLoading()
-		  }
-        })
-      }
-    })
-  },
-  onLoad: function() {
-    var that = this;
-    // 查看是否授权
-    wx.getSetting({
-      success: function(res) {
-        if (res.authSetting['scope.userInfo']) {
-          wx.showLoading({
-            title: 'aaaaa',
-            mask: true,
-          })
-          wx.getUserInfo({
-            success: function(data) {
-              // 用户已经授权过,不需要显示授权页面,所以不需要改变 isHide 的值
-              // 根据自己的需求有其他操作再补充
-              // 我这里实现的是在用户授权成功后，调用微信的 wx.login 接口，从而获取code
-              let loginFlag = wx.getStorageSync('jwt_token');
-              if (loginFlag) {
-                wx.checkSession({
-                  success: function(res) {
-                    console.log("1233")
-                    that.getGroupsInfo()
-                  },
-                  fail: function() {
-                    // this.doLogin()
-                    that.doLogin(data)
-                  }
-                });
-              } else {
-                // 登录
-                // this.doLogin()
-                this.doLogin(data)
-
-              }
-            }
-          });
-        } else {
-          // 用户没有授权
-          // 改变 isHide 的值，显示授权页面
-          that.setData({
-            isHide: true
-          });
-        }
-      }
-    });
-  },
-  onShow: function() {
-    let loginFlag = wx.getStorageSync('jwt_token');
-    if (loginFlag) {
-      var that = this;
-      that.getGroupsInfo()
-    }
-  },
-  bindGetUserInfo: function(e) {
-    if (e.detail.userInfo) {
-      //用户按了允许授权按钮
-      var that = this;
-      // 获取到用户的信息了，打印到控制台上看下
-      console.log("用户的信息如下：");
-      console.log(e.detail.userInfo);
-      //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
-      that.setData({
+    data: {
+        show: false,
+        motto: 'Hello World',
+        userInfo: {},
+        groupsInfo: [],
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        joined: [{
+            name: "嗯",
+            image: "/images/userinfo_bg.png",
+            number: 0,
+            title: "每日英语"
+        }],
+        //判断小程序的API，回调，参数，组件等是否在当前版本可用。
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
         isHide: false
-      });
-      this.doLogin(e.detail)
-      console.log(e)
-    } else {
-      //用户按了拒绝按钮
-      wx.showModal({
-        title: '警告',
-        content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
-        showCancel: false,
-        confirmText: '返回授权',
-        success: function(res) {
-          // 用户没有授权成功，不需要改变 isHide 的值
-          if (res.confirm) {
-            console.log('用户点击了“返回授权”');
-          }
+    },
+    //事件处理函数
+    onChange(event) {
+        wx.showToast({
+            title: 'cccc',
+            icon: 'none'
+        });
+    },
+    moreBtnClicked: function(e) {
+        console.log(e)
+        var that = this
+        // wx.showToast({
+        //   title: e.currentTarget.dataset.item.title
+        // })
+        var jwt = wx.getStorageSync('jwt_token')
+        wx.request({
+            url: 'http://127.0.0.1:3000/api/v1/group/delete',
+            data: {
+                "group_id": e.currentTarget.dataset.item.id,
+                "owner_id": e.currentTarget.dataset.item.owner_id
+            },
+            header: {
+                'content-type': 'application/json',
+                'authorization': jwt,
+            },
+            method: 'POST',
+            dataType: 'json',
+            responseType: 'text',
+            success: function(res) {
+                console.log(res)
+                that.getGroupsInfo()
+            },
+            fail: function(res) {},
+            complete: function(res) {},
+        })
+
+        this.popupOpen();
+    },
+    popupOpen() {
+        this.setData({
+            show: true
+        })
+    },
+    popupClose() {
+        this.setData({
+            show: false
+        })
+    },
+    getGroupsInfo: function() {
+        var that = this;
+        var jwt = wx.getStorageSync('jwt_token')
+        console.log("11111")
+        wx.request({
+            url: 'http://127.0.0.1:3000/api/v1/group/list',
+            data: '',
+            header: {
+                'content-type': 'application/json',
+                'authorization': jwt,
+            },
+            method: 'GET',
+            dataType: 'json',
+            responseType: 'text',
+            success: function(res) {
+                console.log(res)
+                that.setData({
+                    groupsInfo: res.data.data.own_groups
+                })
+                console.log(res.data.data.own_groups)
+            },
+            fail: function(res) {},
+            complete: function(res) {
+                wx.hideLoading()
+            },
+        })
+    },
+    doLogin: function(data) {
+        var that = this
+        console.log(data)
+        wx.login({
+            success: res => {
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                // console.log(res.code)
+                wx.request({
+                    url: 'http://127.0.0.1:3000/api/v1/login',
+                    method: "POST",
+                    data: {
+                        "code": res.code,
+                        "userInfo": data.userInfo,
+                        "rawData": data.rawData,
+                        "signature": data.signature,
+                        "encryptedData": data.encryptedData,
+                        "iv": data.iv
+                    },
+                    header: {
+                        'content-type': 'application/json'
+                    },
+                    success: function(res) {
+                        //从数据库获取用户信息
+                        console.log(res)
+                        console.log(res.data.data["jwt_token"])
+                        wx.setStorageSync("jwt_token", "Bearer " + res.data.data["jwt_token"])
+                        console.log("插入小程序登录用户信息成功！");
+                        that.getGroupsInfo()
+                    },
+                    fail: function(res) {
+                        wx.hideLoading()
+                        wx.showToast({
+                            title: '登陆失败',
+                        })
+                    }
+                })
+            }
+        })
+    },
+    onLoad: function() {
+        var that = this;
+        // 查看是否授权
+        wx.getSetting({
+            success: function(res) {
+                if (res.authSetting['scope.userInfo']) {
+                    wx.showLoading({
+                        title: 'aaaaa',
+                        mask: true,
+                    })
+                    wx.getUserInfo({
+                        success: function(data) {
+                            // 用户已经授权过,不需要显示授权页面,所以不需要改变 isHide 的值
+                            // 根据自己的需求有其他操作再补充
+                            // 我这里实现的是在用户授权成功后，调用微信的 wx.login 接口，从而获取code
+                            let loginFlag = wx.getStorageSync('jwt_token');
+                            if (loginFlag) {
+                                wx.checkSession({
+                                    success: function(res) {
+                                        console.log("1233")
+                                        that.getGroupsInfo()
+                                    },
+                                    fail: function() {
+                                        // this.doLogin()
+                                        that.doLogin(data)
+                                    }
+                                });
+                            } else {
+                                // 登录
+                                // this.doLogin()
+                                console.log("111")
+                                console.log(data)
+                                that.doLogin(data)
+
+                            }
+                        }
+                    });
+                } else {
+                    // 用户没有授权
+                    // 改变 isHide 的值，显示授权页面
+                    that.setData({
+                        isHide: true
+                    });
+                }
+            }
+        });
+    },
+    onShow: function() {
+        let loginFlag = wx.getStorageSync('jwt_token');
+        if (loginFlag) {
+            var that = this;
+            that.getGroupsInfo()
         }
-      });
-    }
-  },
+    },
+    bindGetUserInfo: function(e) {
+        if (e.detail.userInfo) {
+            //用户按了允许授权按钮
+            var that = this;
+            // 获取到用户的信息了，打印到控制台上看下
+            console.log("用户的信息如下：");
+            console.log(e.detail.userInfo);
+            //授权成功后,通过改变 isHide 的值，让实现页面显示出来，把授权页面隐藏起来
+            that.setData({
+                isHide: false
+            });
+            this.doLogin(e.detail)
+            console.log(e)
+        } else {
+            //用户按了拒绝按钮
+            wx.showModal({
+                title: '警告',
+                content: '您点击了拒绝授权，将无法进入小程序，请授权之后再进入!!!',
+                showCancel: false,
+                confirmText: '返回授权',
+                success: function(res) {
+                    // 用户没有授权成功，不需要改变 isHide 的值
+                    if (res.confirm) {
+                        console.log('用户点击了“返回授权”');
+                    }
+                }
+            });
+        }
+    },
 })
--- pages/new-check-in/index.js
+++ pages/new-check-in/index.js
@@ -1,90 +1,126 @@
 // pages/list/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        show: false,
+        title: "",
+        loading_show: false
+    },
+    popupClose: function() {
+        this.setData({
+            show: false
+        })
+    },
+    popupOpen: function() {
+        this.setData({
+            show: true
+        })
+    },
+    onChange: function(event) {
+        this.title = event.detail
+    },
+    bindPopupShow: function(e) {
+        this.popupOpen()
+    },
+    bindCreateGroup: function(e) {
+        var that = this
+        console.log(that.title)
+        if (!this.title) {
+            return
+        }
+        var jwt = wx.getStorageSync('jwt_token')
+        console.log(jwt)
+        wx.showLoading({
+            title: '',
+        })
+        wx.request({
+            url: 'http://127.0.0.1:3000/api/v1/group',
+            data: {
+                "title": that.title
+            },
+            header: {
+                'content-type': 'application/json',
+                'authorization': jwt,
+            },
+            method: "POST",
+            // dataType: 'json',
+            // responseType: 'text',
+            success: function(res) {
+                console.log(res)
+                wx.showToast({
+                    title: 'aaaa',
+                })
+                that.popupClose()
+            },
+            fail: function(res) {
+                console.debug(res)
+                wx.showToast({
+                    title: 'aaaa',
+                })
 
-  bindCreateGroup:function(e){
-    console.log(e.currentTarget.dataset)
-    // e.currentTarget.dataset.item.name
-    var jwt = wx.getStorageSync('jwt_token')
-    console.log(jwt)
-    wx.request({
-      url: 'http://127.0.0.1:3000/api/v1/group',
-      data: {"title":"今日英语"},
-      header: {
-        'content-type': 'application/json',
-        'authorization': jwt,
-      },
-      method: "POST",
-      // dataType: 'json',
-      // responseType: 'text',
-      success: function(res) {
-        console.log(res)
-      },
-      fail: function(res) {
-        console.debug(res)
-      },
-      complete: function(res) {},
-    })
-  },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
-  },
+                that.popupClose()
+            },
+            complete: function(res) {
+                wx.hideLoading()
+            },
+        })
+    },
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
 })
